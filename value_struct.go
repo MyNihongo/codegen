d5da5137d9824590be1d234d5354455590aefc3a
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-11 14:19:49 +0300

    create struct initialisation

---
+++ value_struct.go
@@ -0,0 +1,61 @@
+package codegen
+
+import "strings"
+
+type structValue struct {
+	name      string
+	props     []*StructPropertyValue
+	isAddress bool
+}
+
+type StructPropertyValue struct {
+	name string
+	val  Value
+}
+
+// InitStruct creates a new struct initialisation
+func InitStruct(structName string, properties ...*StructPropertyValue) *structValue {
+	return &structValue{
+		name:  structName,
+		props: properties,
+	}
+}
+
+// Address returns a pointer to the initialised struct
+func (s *structValue) Address() *structValue {
+	s.isAddress = true
+	return s
+}
+
+// PropValue creates a new property with its value
+func PropValue(propertyName string, propertyValue Value) *StructPropertyValue {
+	return &StructPropertyValue{
+		name: propertyName,
+		val:  propertyValue,
+	}
+}
+
+func (s *structValue) writeValue(sb *strings.Builder) {
+	if s.isAddress {
+		sb.WriteByte('&')
+	}
+
+	sb.WriteString(s.name)
+	sb.WriteByte('{')
+
+	for i, prop := range s.props {
+		if i != 0 {
+			sb.WriteByte(',')
+		}
+
+		sb.WriteString(prop.name)
+		sb.WriteByte(':')
+		prop.val.writeValue(sb)
+	}
+
+	sb.WriteByte('}')
+}
+
+func (s *structValue) isPointer() bool {
+	return false
+}
