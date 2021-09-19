b3e1c47e461da5c711308d33508d480f88fda093
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-19 12:03:34 +0300

    tests for defer

--- stmt_defer_test.go
+++ stmt_defer_test.go
@@ -27,3 +27,33 @@ func TestDeferQualFuncCall(t *testing.T) {
 
 	assert.Equal(t, want, sb.String())
 }
+
+func TestDeferFuncArgs(t *testing.T) {
+	const want = `defer myFunc(a)
+`
+	var sb strings.Builder
+	Defer(FuncCall("myFunc").Args(Identifier("a"))).
+		writeStmt(&sb)
+
+	assert.Equal(t, want, sb.String())
+}
+
+func TestDeferFuncFunc(t *testing.T) {
+	const want = `defer myFunc().MyFunc()
+`
+	var sb strings.Builder
+	Defer(FuncCall("myFunc").Call("MyFunc")).
+		writeStmt(&sb)
+
+	assert.Equal(t, want, sb.String())
+}
+
+func TestDeferFieldCall(t *testing.T) {
+	const want = `defer a.b.myFunc(b)
+`
+	var sb strings.Builder
+	Defer(Identifier("a").Field("b").Call("myFunc").Args(Identifier("b"))).
+		writeStmt(&sb)
+
+	assert.Equal(t, want, sb.String())
+}
