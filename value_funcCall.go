5f61d516304bb0f910966bf2a3cb8f72e60493bc
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-19 10:00:17 +0300

    rename to funcCall

--- value_funcCall.go
+++ value_funcCall.go
@@ -2,59 +2,59 @@ package codegen
 
 import "strings"
 
-type qualNameFuncValue struct {
+type funcCallValue struct {
 	alias string
 	name  string
-	isPtr bool
 	args  []Value
+	isPtr bool
 }
 
 // FuncCall creates a new function call
-func FuncCall(name string) *qualNameFuncValue {
+func FuncCall(name string) *funcCallValue {
 	return newFuncCall("", name)
 }
 
 // QualFuncCall creates a new function call with a package alias
-func QualFuncCall(alias, name string) *qualNameFuncValue {
+func QualFuncCall(alias, name string) *funcCallValue {
 	return newFuncCall(alias, name)
 }
 
 // Pointer turns the value to a pointer type
-func (q *qualNameFuncValue) Pointer() *qualNameFuncValue {
+func (q *funcCallValue) Pointer() *funcCallValue {
 	q.isPtr = true
 	return q
 }
 
 // Args appeng argument for the function call
-func (q *qualNameFuncValue) Args(args ...Value) *qualNameFuncValue {
+func (q *funcCallValue) Args(args ...Value) *funcCallValue {
 	q.args = args
 	return q
 }
 
 // Field appends a new field getter after the function call
-func (q *qualNameFuncValue) Field(fieldName string) *fieldValue {
+func (q *funcCallValue) Field(fieldName string) *fieldValue {
 	return newField(q, fieldName)
 }
 
 // Call appends a new function call after the function call
-func (q *qualNameFuncValue) Call(funcName string) *callValue {
+func (q *funcCallValue) Call(funcName string) *callValue {
 	return newCallValue(q, funcName)
 }
 
-func newFuncCall(alias, name string) *qualNameFuncValue {
-	return &qualNameFuncValue{
+func newFuncCall(alias, name string) *funcCallValue {
+	return &funcCallValue{
 		name:  name,
 		alias: alias,
 		args:  make([]Value, 0),
 	}
 }
 
-func (q *qualNameFuncValue) writeStmt(sb *strings.Builder) bool {
+func (q *funcCallValue) writeStmt(sb *strings.Builder) bool {
 	q.writeValue(sb)
 	return true
 }
 
-func (q *qualNameFuncValue) writeValue(sb *strings.Builder) {
+func (q *funcCallValue) writeValue(sb *strings.Builder) {
 	if q.isPtr {
 		sb.WriteByte('*')
 	}
@@ -63,6 +63,6 @@ func (q *qualNameFuncValue) writeValue(sb *strings.Builder) {
 	writeFuncCall(sb, q.name, q.args)
 }
 
-func (q *qualNameFuncValue) isPointer() bool {
+func (q *funcCallValue) isPointer() bool {
 	return q.isPtr
 }
