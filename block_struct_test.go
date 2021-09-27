b3800dc034b6d729db4006b1df6f68577aa20ae0
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-27 21:59:27 +0300

    tests for pointers

--- block_struct_test.go
+++ block_struct_test.go
@@ -48,17 +48,21 @@ prop alias.MyType
 
 func TestStructAddProp(t *testing.T) {
 	const want = `type myStruct struct {
-prop1 string
+prop1 *string
 prop2 string
-prop3 string
+prop3 *string
 prop4 string
-prop5 string
+prop5 *string
 }
 `
 	decl := newStruct("myStruct")
 
 	for i := 0; i < 5; i++ {
-		decl.AddProp(fmt.Sprintf("prop%d", i+1), "string")
+		prop := decl.AddProp(fmt.Sprintf("prop%d", i+1), "string")
+
+		if i%2 == 0 {
+			prop.Pointer()
+		}
 	}
 
 	var sb strings.Builder
@@ -69,18 +73,22 @@ prop5 string
 
 func TestStructQualAddProp(t *testing.T) {
 	const want = `type myStruct struct {
-	prop1 alias.MyType1
+	prop1 *alias.MyType1
 	prop2 alias.MyType2
-	prop3 alias.MyType3
+	prop3 *alias.MyType3
 	prop4 alias.MyType4
-	prop5 alias.MyType5
+	prop5 *alias.MyType5
 }
 `
 	decl := newStruct("myStruct")
 
 	for i := 0; i < 5; i++ {
 		name, typ := fmt.Sprintf("prop%d", i+1), fmt.Sprintf("MyType%d", i+1)
-		decl.AddQualProp(name, "alias", typ)
+		prop := decl.AddQualProp(name, "alias", typ)
+
+		if i%2 == 0 {
+			prop.Pointer()
+		}
 	}
 
 	var sb strings.Builder
