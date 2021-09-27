44b3aed10229a6a741b358d68eeffa9a635ed838
mihails.kuzmins <mihails.kuzmins@daytongroup.lv>
2021-09-27 21:57:06 +0300

    add helper methods AddProp and AddQualProp

--- block_struct_test.go
+++ block_struct_test.go
@@ -58,12 +58,33 @@ prop5 string
 	decl := newStruct("myStruct")
 
 	for i := 0; i < 5; i++ {
-		newProp := Property(fmt.Sprintf("prop%d", i+1), "string")
-		decl.AddProp(newProp)
+		decl.AddProp(fmt.Sprintf("prop%d", i+1), "string")
 	}
 
 	var sb strings.Builder
 	decl.write(&sb)
 
 	assert.Equal(t, want, sb.String())
+}
+
+func TestStructQualAddProp(t *testing.T) {
+	const want = `type myStruct struct {
+	prop1 alias.MyType1
+	prop2 alias.MyType2
+	prop3 alias.MyType3
+	prop4 alias.MyType4
+	prop5 alias.MyType5
+}
+`
+	decl := newStruct("myStruct")
+
+	for i := 0; i < 5; i++ {
+		name, typ := fmt.Sprintf("prop%d", i+1), fmt.Sprintf("MyType%d", i+1)
+		decl.AddQualProp(name, "alias", typ)
+	}
+
+	var sb strings.Builder
+	decl.write(&sb)
+
+	assert.Equal(t, want, formatSb(sb))
 }
