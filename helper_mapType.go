c5402b820237f74d856f67df62967d1d4187e4bb
ミシャ <47413092+MihailsKuzmins@users.noreply.github.com>
2021-11-04 21:09:10 +0200

    create MapTypeDecl

---
+++ helper_mapType.go
@@ -0,0 +1,27 @@
+package codegen
+
+import "strings"
+
+type MapTypeDecl struct {
+	keyType   *TypeDecl
+	valueType *TypeDecl
+}
+
+func MapType(keyType *TypeDecl, valueType *TypeDecl) *MapTypeDecl {
+	if keyType.name.isArray {
+		panic("key type cannot be an array")
+	}
+
+	return &MapTypeDecl{
+		keyType:   keyType,
+		valueType: valueType,
+	}
+}
+
+func (m *MapTypeDecl) wr(sb *strings.Builder) {
+	sb.WriteString("map[")
+	m.keyType.wr(sb)
+	sb.WriteByte(']')
+
+	m.valueType.wr(sb)
+}
